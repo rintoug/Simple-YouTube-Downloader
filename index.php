dbf620d31725a4f9a6ebb691e7e96954fed6a8ae
Rinto <mail.rinto@gmail.com>
2020-08-20 23:50:52 +0400

    Added some validations

--- index.php
+++ index.php
@@ -5,24 +5,29 @@ $downloadLinks ='';
 $error='';
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $videoLink = $_POST['video_link'];
-    $vid = $yt->getYouTubeCode($videoLink);
-    if($vid) {
-        $result = $yt->processVideo($vid);
+    if(!empty($videoLink)) {
+        $vid = $yt->getYouTubeCode($videoLink);
+        if($vid) {
+            $result = $yt->processVideo($vid);
 
-        if($result) {
-            //print_r($result);
-            $info = $result['info'];
-            $downloadLinks = $result['videos'];
+            if($result) {
+                //print_r($result);
+                $info = $result['info'];
+                $downloadLinks = $result['videos'];
 
-            $videoInfo = json_decode($info['player_response']);
+                $videoInfo = json_decode($info['player_response']);
 
-            $title = $videoInfo->videoDetails->title;
-            $thumbnail = $videoInfo->videoDetails->thumbnail->thumbnails{0}->url;
+                $title = $videoInfo->videoDetails->title;
+                $thumbnail = $videoInfo->videoDetails->thumbnail->thumbnails{0}->url;
+            }
+            else {
+                $error = "Something went wrong";
+            }
+
         }
-        else {
-            $error = "Something went wrong";
-        }
-
+    }
+    else {
+        $error = "Please enter a YouTube video URL";
     }
 }
 ?>
